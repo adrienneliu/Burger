85c7b310b293a27b067a0272a73ac957433011c3
Adrienne <iliu.adrienne@gmail.com>
2019-07-07 17:26:08 -0700

    add first function and connect to DOM

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,16 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
+//Setting up routes
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -2,5 +2,12 @@
 
 var orm = require("../config/orm.js");
 
-
+//call ORM functions using specific input for ORM 
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    }
+}
 module.exports = burger;
